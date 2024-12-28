3c1815fbb4746e5015196aee2fa2fcd837a48336
Frank Sera <seracreativo@gmail.com>
2024-12-28 06:09:43 +0100

    Fomrulario

---
+++ Notes/Extras/FomPractice.swift
@@ -0,0 +1,18 @@
+//
+//  FomPractice.swift
+//  Notes
+//
+//  Created by Frank Sera on 28/12/24.
+//
+
+import SwiftUI
+
+struct FomPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FomPractice()
+}
