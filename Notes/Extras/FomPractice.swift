7a34541910749de666bf69b67a174541c47c4c8e
Frank Sera <seracreativo@gmail.com>
2024-12-28 06:10:21 +0100

    estructura formulario

--- Notes/Extras/FomPractice.swift
+++ Notes/Extras/FomPractice.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct FomPractice: View {
+    
+    @State var name : String = ""
+    @State var emailAddress : String = ""
+    @State var details : String = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                TextField("Email", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .textInputAutocapitalization(.never)
+            }
+            Section("Notes") {
+                TextField("Detalles", text: $details)
+                    
+            }
+        }
+        .navigationTitle("Edit Person")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
